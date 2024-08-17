7b2112f86abb8fd1f8f24e3f7180f0ec13b1906d
João Victor <joaovictormdasilva676@gmail.com>
2024-08-17 19:50:52 -0300

    Atualizando as minhas informações

--- portfolio/src/pages/Sobre/index.jsx
+++ portfolio/src/pages/Sobre/index.jsx
@@ -1,11 +1,11 @@
 import styles from "./Sobre.module.css";
 import avatar from "./imagens/WhatsApp Image 2023-07-04 at 21.25.28.jpeg";
-import css from "./imagens/icon-css.svg";
-import html from "./imagens/icon-html.svg";
-import js from "./imagens/icon-js.svg";
-import c from "./imagens/icon-c.svg";
-import react from "./imagens/icon-react.svg";
 import sql from "./imagens/icon-sql.svg";
+import { RiHtml5Fill, RiCss3Fill, RiJavascriptFill, RiReactjsLine } from "react-icons/ri";
+import { FaJava } from "react-icons/fa6";
+import { SiTailwindcss } from "react-icons/si";
+import { IoLogoSass } from "react-icons/io";
+import { BsFiletypeSql } from "react-icons/bs";
 
 function Sobre() {
   return (
@@ -19,26 +19,24 @@ function Sobre() {
           </p>
           <p>Trabalho com desenvolvimento Web</p>
           <p>
-            Como Desenvolvedor Full-Stack, tenho a responsabilidade de
-            desenvolver e manter o front-end e back-end de aplicativos para web.
-            Com a minha experiência em C#, React.js e SQL, sou capaz de criar
-            aplicativos eficientes, escaláveis e de fácil compreensão.
+            Na posição de Desenvolvedor Full-Stack, é minha responsabilidade criar e manter tanto o front-end quanto o back-end de aplicativos web. Com conhecimento em Java, React.js e SQL, posso desenvolver aplicativos eficazes, escaláveis e de fácil compreensão.
           </p>
           <p>
-            Especializado em criação de aplicações dinâmicas e intuitivas,{" "}
-            <br /> com foco na experiência do usuário.
+            Tenho expertise em desenvolver aplicações dinâmicas e intuitivas, sempre priorizando a experiência do usuário.
           </p>
         </div>
       </div>
       <div className={styles.techs}>
         <h3>Techs</h3>
         <div className={styles.icones}>
-          <img src={html} alt="Ícon do Html" className={styles.icon} />
-          <img src={css} alt="Ícon do css" className={styles.icon} />
-          <img src={js} alt="Ícon do js" className={styles.icon} />
-          <img src={react} alt="Ícon do react" className={styles.icon} />
-          <img src={c} alt="Ícon do node" className={styles.icon} />
-          <img src={sql} alt="João victor sql" className={styles.icon} />
+          <RiHtml5Fill size={100} color="#16213E" />
+          <RiCss3Fill size={100} color="#16213E" />
+          <RiJavascriptFill size={120} color="#16213E" />
+          <IoLogoSass size={120} color="#16213E" />
+          <SiTailwindcss size={100} color="#16213E" />
+          <RiReactjsLine size={100} color="#16213E" />
+          <FaJava size={100} color="#16213E" />
+          <BsFiletypeSql size={100} color="#16213E" />
         </div>
       </div>
     </section>
