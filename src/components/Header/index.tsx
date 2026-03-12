2a829ad71ba7bd08997dbf3c3cbf71cf5b811a50
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2026-03-12 20:21:37 -0300

    feature: create style css moule header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import { Burger, Flex, Switch, Title } from "@mantine/core";
-import { IconMoon, IconSunLow } from '@tabler/icons-react';
+import { IconMoon, IconSunLow } from "@tabler/icons-react";
+import styles from "./Header.module.css";
 
 export function Header() {
-    return (
-        <Flex>
-            <Title order={1}>Portfolio</Title>
-            <Switch size="md" onLabel={<IconSunLow />} offLabel={<IconMoon />} />
-            <Burger aria-label="Toggle navigation" title="Hamburger menu" />
-        </Flex>
-    )
+  return (
+    <Flex className={styles.header}>
+      <Title order={1} className={styles.title}>Portfólio</Title>
+      <Flex className={styles.menu}>
+        <Switch size="md" onLabel={<IconSunLow />} offLabel={<IconMoon />} />
+        <Burger aria-label="Toggle navigation" title="Hamburger menu" />
+      </Flex>
+    </Flex>
+  );
 }
